4a47da95d302762ee11e949afc8fae13409662d9
ctrl-q <34975747+ctrl-q@users.noreply.github.com>
2024-11-30 15:59:58 -0500

    hotfix: Stop using gdbus
    Fails on long strings due to the shell args being too long. So I'll instead use my own signal emitter that takes as stdin, which has no limit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import base64
 import json
 import os
+import pickle
 import re
 import subprocess
 import sys
@@ -10,6 +12,7 @@ from typing import Any
 
 import dbus
 import dbus.mainloop.glib
+import dbus.service
 from dbus.lowlevel import MethodCallMessage, MethodReturnMessage
 from gi.repository import GLib
 
@@ -77,6 +80,59 @@ def get_outdir(notification: dict[str, str]) -> Path:
     return default_outdir
 
 
+def to_pickle(payload: dict[str, Any]) -> bytes:
+    def to_native_type(x: Any) -> Any:
+        match x:
+            case dbus.String():
+                return str(x)
+            case dbus.Boolean():
+                return bool(x)
+            case (
+                dbus.Byte()
+                | dbus.UInt16()
+                | dbus.UInt32()
+                | dbus.Int64()
+                | dbus.Int16()
+                | dbus.Int32()
+            ):
+                return int(x)
+            case dbus.Double():
+                return float(x)
+            case dbus.ObjectPath():
+                return str(x)
+            case dbus.Array():
+                if x.signature == dbus.Signature("y"):
+                    return bytearray(x)
+                else:
+                    return [to_native_type(value) for value in x]
+            case dbus.Dictionary():
+                new_data = {}
+                for key in x:
+                    new_data[to_native_type(key)] = to_native_type(x[key])
+                return new_data
+            case _:
+                return x
+
+    return pickle.dumps(dict(zip(payload, map(to_native_type, payload.values()))))
+
+
+def emit_signal():
+    dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+
+    class NotificationSent(dbus.service.Object):
+        @dbus.service.signal(
+            dbus_interface="com.example.DbusNotificationsToJson",
+            signature="s",
+        )
+        def NotificationSent(self, base64_encoded_args: str):
+            pass
+
+    NotificationSent(
+        dbus.SessionBus(),
+        "/com/example/DbusNotificationsToJson/notifications",
+    ).NotificationSent(base64.b64encode(sys.stdin.buffer.read()).decode())
+
+
 def write_to_file(message: MethodReturnMessage):
     if dict_ := MESSAGE_CACHE.pop(
         (message.get_destination(), message.get_sender(), message.get_reply_serial()),
@@ -86,27 +142,26 @@ def write_to_file(message: MethodReturnMessage):
         outdir = get_outdir(dict_)
         outdir.mkdir(parents=True, exist_ok=True)
         outfile = outdir / f"{time.strftime('%Y%m%d-%H%M%S')}-{notification_id}.json"
-        payload = json.dumps(
-            dict_ | {"id": notification_id, "path": str(outfile)}, ensure_ascii=False
-        )
+
+        payload = dict_ | {
+            "id": notification_id,
+            "path": str(outfile),
+        }
+
         # I can't for the life of me figure out how to send a signal
-        # using https://dbus.freedesktop.org/doc/dbus-python/tutorial.html#emitting-signals-with-dbus-service-signal
+        # within the same event loop
         # the program crashes as soon as it tries to emit the signal
         try:
+            # I can't figure out how to pass list (the actions) and dict (the hints) args to notification-tray's signal receiver, so I'll base64-encode a pickle dump of the args
             subprocess.run(
-                [
-                    "gdbus",
-                    "emit",
-                    "--session",
-                    "--signal=com.example.DbusNotificationsToJson.NotificationSent",
-                    "--object-path=/com/example/DbusNotificationsToJson/notifications",
-                    payload,
-                ]
+                ["dbus-to-json", "emit"],
+                input=to_pickle(payload),
             )
+
         except Exception as e:
             print("Error:", e, file=sys.stderr)
         with outfile.open("w") as f:
-            f.write(payload)
+            json.dump(payload, f)
         print(f"Notification written to {outfile}", file=sys.stderr)
 
 
@@ -140,4 +195,7 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) > 1 and sys.argv[-1] == "emit":
+        emit_signal()
+    else:
+        main()
